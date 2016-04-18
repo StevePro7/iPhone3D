139db6b0fe07cbb0131a5de33e2ed303b7ab0769
StevePro7 <steveprozune@hotmail.com>
2016-04-18 20:45:33 +0100

    Update files

--- Chap04/FancyWireframe/Classes/GLView.mm
+++ Chap04/FancyWireframe/Classes/GLView.mm
@@ -15,7 +15,7 @@
     {
         CAEAGLLayer* eaglLayer = (CAEAGLLayer*) self.layer;
         eaglLayer.opaque = YES;
-
+        
         EAGLRenderingAPI api = kEAGLRenderingAPIOpenGLES1;
         m_context = [[EAGLContext alloc] initWithAPI:api];
         
@@ -29,7 +29,7 @@
             return nil;
         }
         
-
+        
         if (api == kEAGLRenderingAPIOpenGLES1) {
             NSLog(@"Using OpenGL ES 1.1");
             m_renderingEngine = FacetedES1::CreateRenderingEngine();
@@ -37,13 +37,13 @@
             NSLog(@"Using OpenGL ES 2.0");
             m_renderingEngine = FacetedES2::CreateRenderingEngine();
         }
-
-       m_applicationEngine = ParametricViewer::CreateApplicationEngine(m_renderingEngine);
-
+        
+        m_applicationEngine = ParametricViewer::CreateApplicationEngine(m_renderingEngine);
+        
         [m_context
-            renderbufferStorage:GL_RENDERBUFFER
-            fromDrawable: eaglLayer];
-                
+         renderbufferStorage:GL_RENDERBUFFER
+         fromDrawable: eaglLayer];
+        
         int width = CGRectGetWidth(frame);
         int height = CGRectGetHeight(frame);
         m_applicationEngine->Initialize(width, height);
@@ -53,10 +53,10 @@
         
         CADisplayLink* displayLink;
         displayLink = [CADisplayLink displayLinkWithTarget:self
-                                     selector:@selector(drawView:)];
+                                                  selector:@selector(drawView:)];
         
         [displayLink addToRunLoop:[NSRunLoop currentRunLoop]
-                     forMode:NSDefaultRunLoopMode];
+                          forMode:NSDefaultRunLoopMode];
     }
     return self;
 }
